8f3c4947013f63b917f537d300d8db704e1c7dcc
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-05-05 19:04:08 +0200

    comienzo atributos

--- funciones.py
+++ funciones.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-f = pd.read_csv("USA_Housing.csv", sep = ',')
+f = pd.read_csv("USA_Housing.csv", sep = ',', header = 0, index_col= 0)
 print(f)
+
+def atributos_dataframe(dataframe):
+    print("Estas son las filas del dataframe:")
+    print(dataframe.index)
+    print("Estas son las columnas del dataframe:")
+    print(dataframe.columns)
+
+atributos_dataframe(f)
+print("")
