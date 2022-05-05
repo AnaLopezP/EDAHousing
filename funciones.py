01d236c5a4d2dab29686a3f0a3ef6a9b876e6a6b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-05-05 19:18:19 +0200

    traducir

--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-f = pd.read_csv("USA_Housing.csv", sep = ',', header = 0, index_col= 0)
+f = pd.read_csv("USA_Housing.csv", sep = ',')
 print(f)
 
 def atributos_dataframe(dataframe):
@@ -9,5 +9,12 @@ def atributos_dataframe(dataframe):
     print("Estas son las columnas del dataframe:")
     print(dataframe.columns)
 
+def traduccion(dataframe, lista):
+    dataframe = dataframe.rename(columns = lista)
+    return dataframe
+
 atributos_dataframe(f)
-print("")
+print("En la lista de las columnas podemos observar que todos los nombres estan en ingles.")
+print("Para hacer el analisis estadistico mas sencillo, traduciremos estos nombres al español:")
+traducido =  {'Avg. Area Income': 'Ganancia Media','Avg. Area House Age':'Edad Casa Media', 'Avg. Area Number of Rooms':'Num Habitaciones Medio', 'Avg. Area Number of Bedrooms': 'Num HabitCama Medio', 'Area Population': 'Poblacion en Area', 'Price':'Precio', 'Address':'Direccion'}
+print(traduccion(f, traducido))
