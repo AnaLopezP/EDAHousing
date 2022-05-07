26df71dc7075dbec8fd9070150f0789757041cab
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-05-07 14:37:38 +0200

    ñl

--- Housing/funciones.py
+++ Housing/funciones.py
@@ -13,7 +13,26 @@ def traduccion(dataframe, lista):
     dataframe = dataframe.rename(columns = lista)
     return dataframe
 
+f = pd.read_csv("USA_Housing.csv", sep = ',')
+print("Este es el dataframe inicial:")
+print(f)
+print("\n")
+atributos_dataframe(f)
+print("\n")
+print("En la lista de las columnas podemos observar que todos los nombres estan en ingles.")
+print("Para hacer el analisis estadistico mas facilmente, traduciremos estos nombres al español:")
+print('\n')
+traducido =  {'Avg. Area Income': 'Ganancia Media','Avg. Area House Age':'Edad Casa Media', 'Avg. Area Number of Rooms':'Num Habitaciones Medio', 'Avg. Area Number of Bedrooms': 'Num HabitCama Medio', 'Area Population': 'Poblacion en Area', 'Price':'Precio', 'Address':'Direccion'}
+f2 = traduccion(f, traducido)
+print("\n")
+print("voy a exportar el dataframe a un excel. De esta manera vere mas facil e intuitivamente la tabla.")
+f2.to_excel("Housing_traducido.xlsx")
 
-
-
-
+print("Voy a hacer distintas graficas con las variables iniciales")
+print("1) histograma de la ganancia")
+fig = plt.figure()
+ganancia_media = f2["Ganancia Media"]
+frecuencias_ganancias = f2.value_counts()
+plt.hist(ganancia_media, frecuencias_ganancias, color = "b")
+plt.savefig("Histograma Ganancias.jpg")
+plt.show
